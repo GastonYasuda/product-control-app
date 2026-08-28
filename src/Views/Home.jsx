c371b8bb2d7bf37b2788359992dc8bde6c8ecb1d
GastonYasuda <gastonyasuda@gmail.com>
2026-08-27 21:13:49 -0300

    React Bootstrap Cards 2 columns

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,5 +1,13 @@
 import React, { useContext, useEffect } from 'react'
 import { DataProductApi } from '../Context/DataBaseProductApi'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+
+
+
 
 const Home = () => {
 
@@ -13,20 +21,40 @@ const Home = () => {
     }, [getAllProducts])
 
     return (
-        <>
-            {getAllProducts.length > 0 &&
-                getAllProducts.map((product, i) => {
-                    return (
-                        <div key={i}>
-                            <h6>{product.name}</h6>
-                            <img src={product.image} alt={`${product.name} image`} />
-                            <p>{product.supplier}</p>
-                            <p>${product.price}</p>
-                        </div>
-                    )
-                })
-            }
-        </>
+
+        <Row xs={2} md={2} className="g-4">
+            {getAllProducts.map((product, i) => (
+                <Col key={i}>
+                    <Card className="h-100">
+                        <Card.Img
+                            variant="top"
+                            src={product.image}
+                            style={{
+                                height: '200px',
+                                objectFit: 'contain'
+                            }}
+                        />
+
+                        <Card.Body>
+                            <Card.Title>{product.name}</Card.Title>
+                            <Card.Text>{product.supplier}</Card.Text>
+                            <Card.Subtitle>${product.price}</Card.Subtitle>
+                            <Card.Text>Stock: {product.stock}</Card.Text>
+
+                            <Form style={{ display: 'flex' }}>
+                                <Form.Control
+                                    type="number"
+                                    placeholder="0"
+                                />
+                                <Button type="submit">
+                                    Agregar
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ProductControlApi from './Context/ProductControlApi.jsx'
 import DataBaseProductApi from './Context/DataBaseProductApi.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
