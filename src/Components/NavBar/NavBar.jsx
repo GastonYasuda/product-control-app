ff8dbfbcd9109b996a833d81174e346aff586fd5
GastonYasuda <gastonyasuda@gmail.com>
2026-08-29 17:35:48 -0300

    Navbar Links

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,31 +1,34 @@
 import React from 'react'
 import './navbar.css'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <div className='navBar_container m-auto fixed-bottom p-2 d-flex justify-content-around'>
-            <span className='d-flex flex-column'>
+            <Link to={'/'} className='d-flex flex-column'>
                 <span className="material-symbols-outlined navbar_icon">
                     splitscreen
                 </span>
                 <span className='navbar_txt'>Productos</span>
-            </span>
+            </Link>
 
-            <span className='d-flex flex-column'>
+            <Link to={'/category'} className='d-flex flex-column'>
                 <span className="material-symbols-outlined navbar_icon">
                     category
                 </span>
                 <span className='navbar_txt'>Categoria</span>
-            </span>
+            </Link>
 
-            <span className='d-flex flex-column'>
+            <Link to={'/order'} className='d-flex flex-column'>
                 <span className="material-symbols-outlined navbar_icon">
                     order_approve
                 </span>
                 <span className='navbar_txt'>Repo</span>
-            </span>
+            </Link>
 
-            <span className='d-flex flex-column'>
+            <span className='d-flex flex-column' onClick={() => {
+                console.log('tiene que salir un sider para hacer logout');
+            }}>
                 <span className="material-symbols-outlined navbar_icon">
                     account_circle
                 </span>
