4374da53ce0f06d561ddf2b4852e6bf503012f80
GastonYasuda <gastonyasuda@gmail.com>
2026-09-14 23:43:41 -0300

    depo InfoCard Style

--- src/Components/InfoCards/InfoCards.jsx
+++ src/Components/InfoCards/InfoCards.jsx
@@ -7,7 +7,7 @@ const InfoCards = ({ cardParams }) => {
         <div className='infoCard_component mt-3'>
 
             {cardParams.map((card, i) => (
-                <div className='infoCard_body p-3' key={i}>
+                <div className='infoCard_body px-2' key={i}>
                     <div className='infoCard_body_item bg-light rounded p-3 d-flex flex-column'>
 
                         <section className='d-flex flex-row justify-content-around align-items-center'>
